81f2203dd5179fa7170a8107327835da509ccde4
hirammtz2003 <166641567+hirammtz2003@users.noreply.github.com>
2025-11-14 13:34:22 -0600

    Update JugandoArreglos.java

--- JugandoArreglos/src/main/java/com/mycompany/jugandoarreglos/JugandoArreglos.java
+++ JugandoArreglos/src/main/java/com/mycompany/jugandoarreglos/JugandoArreglos.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-/*Probando Webhook*/
+/*probando webhook*/
 
 package com.mycompany.jugandoarreglos;
 
